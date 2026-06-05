1a8cbf7d3669859ee45c368638f41310da48082d
towtu <fatowtu123@gmail.com>
2026-06-05 17:02:12 +0800

    Add React app shell and global styling

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Browse from './pages/Browse';
+import MovieDetail from './pages/MovieDetail';
+import Search from './pages/Search';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/browse/:rowId" element={<Browse />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="/tv/:id" element={<MovieDetail isTV={true} />} />
+          <Route path="/anime/:id" element={<MovieDetail isAnime={true} />} />
+          <Route path="/search" element={<Search />} />
+          {/* Add a fallback route */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
